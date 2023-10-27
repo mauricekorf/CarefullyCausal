6d3da78bc58b76b327b09bba7aac60856a152ff7
mauricekorf <mauricekorf@hotmail.com>
2023-10-27 09:54:38 +0200

    Change help file description of CarefullyCausal function

--- R/CarefullyCausal.R
+++ R/CarefullyCausal.R
@@ -1,6 +1,6 @@
 #' CarefullyCausal
 #'
-#' Performs outcome regression, IPTW, S-standardization, T-standardization and TMLE.
+#' \pkg{CarefullyCausal} performs outcome regression, IPTW, S-standardization, T-standardization and TMLE.
 #' It provides the estimand, estimates, discusses the assumptions and provides diagnostics including interpretations.
 #' It can be used when having a fixed-exposure, outcome is either dichotomous or continuous and when
 #' exposure is dichotomous, multi-value or continuous.
